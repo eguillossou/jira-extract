6e5df2f067d53e3879baa6ae75828dd50caf9922
eguillossou <eguillossou@talend.com>
2019-01-26 11:14:16 +0100

    wip on ticket added after start of sprint

--- jira-extract.py
+++ jira-extract.py
@@ -16,7 +16,7 @@ from dateutil.parser import *
 #tdc board = 217
 TDC_JIRA_BOARD_ID = 217
 PATH_EXCEL_FILE = "c:\\Users\\eguillossou\\"
-EXCEL_FILE_NAME = "jira-report" 
+EXCEL_FILE_NAME = "jira-report"
 
 def log(str):
     print(str)
@@ -33,17 +33,18 @@ def get_active_sprint(handler_jira):
         print("Future sprint {} selected".format(active_sprint[0].name))
     else:
         print("Active sprint {} selected".format(active_sprint[0].name))
+    return active_sprint[0]
 
-    return active_sprint[0].name
-
-def get_selected_sprint_number(arg_sp, handler_jira):
+def get_selected_sprint_number(arg_sp, sprint_details_in):
     if(arg_sp is not None):
         return int(arg_sp)
     else:
-        return int(re.findall(r"\d+",get_active_sprint(handler_jira))[0])
+        return int(re.search(r"\d+",str(sprint_details_in)).group())
 
-def get_sprint_start_date(field_11070):
-    return re.findall(r"startDate=[^,]+",str(field_11070))[0].replace("startDate=","")
+def get_sprint_start_date(sprint_details_in, field_11070):
+    regexp=re.compile('name={}[ ]([0-9]+)[,]startDate=([^,]+)'.format(get_selected_sprint_number(None, sprint_details_in)))
+    
+    return re.findall(regexp,str(field_11070))[0].replace("startDate=","")
 
 def construct_jql_query(sp_nb, handler_jira):
     jql_qry='project = TDC AND issuetype in (Bug, "New Feature", "Work Item") AND Sprint = "TDC Sprint {}" ORDER BY labels ASC, RANK'.format(sp_nb)
@@ -59,15 +60,17 @@ def if_already_started(sprints_list_in,sprint_number_in):
     else:
         return(False)
 
-def if_added_after_started(issue_ids_in, sprint_info_in):
+def if_added_after_started(issue_ids_in, sprint_details_in, customfield_11070):
     starting_sprint_date = "2019-01-15T02:31:56.000-0600"
-    starting_sprint_date = get_sprint_start_date(sprint_info_in)
-    log(starting_sprint_date)
+    # if(check_sprint_status() is Active)
+    starting_sprint_date = '<null>'
+    get_sprint_start_date(sprint_details_in,customfield_11070)
+    if(starting_sprint_date == '<null>'):
+        return(False)
     
-    # if(starting_sprint_date > inclusion_date_in_sprint):
-
-    log("key:"+issue_ids_in.key)
-    # if(str(issue_ids_in.key) == "TDC-1689"): import pdb;pdb.set_trace()
+    log("key:"+issue_ids_in.key+" starting date of sprint: "+starting_sprint_date)
+    # if(str(issue_ids_in.key) == "TDC-1689"): 
+    # import pdb;pdb.set_trace()
     for history in issue_ids_in.changelog.histories:
         if(history.created is not None and parse(history.created) < parse(starting_sprint_date)):
             log("{} jira ticket added to current sprint after sprint was started : {}".format(str(issue_ids_in.key), starting_sprint_date))
@@ -88,9 +91,9 @@ def construct_datas(header_list_in, values_issues_in):
 
     return issues_according_to_header_list
 
-def fillIT(issue_ids_in, sprint_number_in):
-    sprint_info = issue_ids_in.fields.customfield_11070
-    sprint_list = parse_sprints(sprint_info)
+def fillIT(issue_ids_in, sprint_details_in, sprint_number_in):
+    customfield_11070 = issue_ids_in.fields.customfield_11070
+    sprint_list = parse_sprints(customfield_11070)
 
     return [issue_ids_in.fields.issuetype.name, 
             issue_ids_in.key, 
@@ -100,7 +103,7 @@ def fillIT(issue_ids_in, sprint_number_in):
             issue_ids_in.fields.priority.name, 
             sprint_list, 
             if_already_started(sprint_list, sprint_number_in),
-            if_added_after_started(issue_ids_in, sprint_info)]
+            if_added_after_started(issue_ids_in, sprint_details_in, customfield_11070)]
 
 def fill_cell(ws_in, line_in, col_in, value_in):
     ws_in.cell(row=line_in, column=col_in).value = value_in
@@ -130,7 +133,8 @@ if __name__ == '__main__':
 
     jira_options={'server': 'https://jira.talendforge.org/','agile_rest_path': 'agile'}
     jira=JIRA(options=jira_options,basic_auth=('eguillossou',os.environ['PASSWORD_JIRA']))
-    sprint_number =  get_selected_sprint_number(arguments().parse_args().sp, jira)
+    sprint_details=get_active_sprint(jira)
+    sprint_number=get_selected_sprint_number(arguments().parse_args().sp, sprint_details)
     issues=jira.search_issues(construct_jql_query(sprint_number,jira), startAt=0, maxResults=500, validate_query=True, fields=None, expand="changelog", json_result=None)
 
     wb = Workbook()
@@ -140,7 +144,7 @@ if __name__ == '__main__':
     # Issue type	Issue key	Summary	Custom field (Story Points)	Status	Priority	Sprint	Already started before	Added after started
     header_list = ["Issue type", "Issue key", "Summary", "Custom field (Story Points)", "Status", "Priority", "Sprint", "Already started before", "Added after started"]
     
-    values_issues = [ fillIT(issue_ids, sprint_number) for issue_ids in issues ]
+    values_issues = [ fillIT(issue_ids, sprint_details, sprint_number) for issue_ids in issues ]
     issues_line = construct_datas(header_list, values_issues)
 
     [fill_headers_and_values(ws1, header_list, lineidx, issues_line) for lineidx in range(len(issues))]
